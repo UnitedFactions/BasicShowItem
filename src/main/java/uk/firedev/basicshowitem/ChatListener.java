e4e85f6d7fd8767236c5951f9003666dea3ee0bb
FireML <fireml@firedev.uk>
2026-08-18 03:25:03 +0100

    Switch to MiniMessage tags and add more choices

--- src/main/java/uk/firedev/basicshowitem/ChatListener.java
+++ src/main/java/uk/firedev/basicshowitem/ChatListener.java
@@ -2,46 +2,30 @@ package uk.firedev.basicshowitem;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import uk.firedev.basicshowitem.tag.ItemTagResolver;
 
 public class ChatListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onChat(AsyncChatEvent event) {
-        if (!event.getPlayer().hasPermission("basicshowitem.use")) {
+        Player player = event.getPlayer();
+        if (!player.hasPermission("basicshowitem.use")) {
             return;
         }
-        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
-        Component hover = buildHover(item);
-        Component message = event.message()
-            .replaceText(builder -> builder.matchLiteral("[i]").replacement(hover))
-            .replaceText(builder -> builder.matchLiteral("[item]").replacement(hover));
+        MiniMessage mm = MiniMessage.miniMessage();
+        String parsedMessage = ItemTagResolver.replaceVariables(
+            mm.serialize(event.message())
+        );
+        Component message = mm.deserialize(parsedMessage, ItemTagResolver.get(player));
         event.message(message);
-    }
-
-    private Component buildHover(@NotNull ItemStack item) {
-        if (item.isEmpty()) {
-            return Component.text("[Air]");
-        }
-        return Component.text()
-            .append(Component.text("["))
-            .append(retrieveDisplayName(item))
-            .append(Component.text("]"))
-            .hoverEvent(item)
-            .build();
-    }
-
-    private Component retrieveDisplayName(@NotNull ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null) {
-            return Component.text("Air");
-        }
-        Component display = meta.displayName();
-        return display == null ? Component.translatable(item) : display;
     }
 
 }
---
+++ src/main/java/uk/firedev/basicshowitem/tag/ItemTagResolver.java
@@ -0,0 +1,71 @@
+package uk.firedev.basicshowitem.tag;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
+
+public class ItemTagResolver {
+
+    public static TagResolver get(@NonNull Player player) {
+        return TagResolver.resolver(
+            resolver(player, "item", EquipmentSlot.HAND),
+            resolver(player, "i", EquipmentSlot.HAND),
+            resolver(player, "hand", EquipmentSlot.HAND),
+            resolver(player, "offhand", EquipmentSlot.OFF_HAND),
+            resolver(player, "head", EquipmentSlot.HEAD),
+            resolver(player, "chest", EquipmentSlot.CHEST),
+            resolver(player, "legs", EquipmentSlot.LEGS),
+            resolver(player, "feet", EquipmentSlot.FEET)
+        );
+    }
+
+    public static String replaceVariables(@NonNull String string) {
+        return string
+            .replace("[item]", "<item>")
+            .replace("[hand]", "<hand>")
+            .replace("[offhand]", "<offhand>")
+            .replace("[head]", "<head>")
+            .replace("[chest]", "<chest>")
+            .replace("[legs]", "<legs>")
+            .replace("[feet]", "<feet>");
+    }
+
+    private static TagResolver resolver(@NonNull final Player player, @NonNull final String name, @NonNull final EquipmentSlot slot) {
+        return TagResolver.resolver(name, ((argumentQueue, context) ->
+            Tag.selfClosingInserting(buildHover(player.getInventory().getItem(slot)))
+        ));
+    }
+
+    /**
+     * Manually builds the hover event to avoid legacy chat plugins parsing the raw translation.
+     * <p>
+     * If a legacy chat plugin is present, the hover data will be lost, but the item name will be shown.
+     */
+    private static Component buildHover(@NotNull ItemStack item) {
+        if (item.isEmpty()) {
+            return Component.text("[Air]");
+        }
+        return Component.text()
+            .append(Component.text("["))
+            .append(retrieveDisplayName(item))
+            .append(Component.text("]"))
+            .hoverEvent(item)
+            .build();
+    }
+
+    private static Component retrieveDisplayName(@NotNull ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) {
+            return Component.text("Air");
+        }
+        Component display = meta.displayName();
+        return display == null ? Component.translatable(item) : display;
+    }
+
+}
