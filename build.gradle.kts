61f05cd2f5b7773b83a1cb9ca9cb2484b52011ec
FireML <fireml@firedev.uk>
2026-08-18 03:27:34 +0100

    Use gradle.properties for project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 }
 
 group = "uk.firedev"
-version = "1.0.3"
+version = project.property("project-version") as String
 description = "Show your held item in chat"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
